5894034f5df73c9552e4e0e9596739aedba76711
HarishST <harishstprojects@gmail.com>
2020-12-13 20:12:55 +0530

    Initial Commit

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+// Hero Bannner - Swiper JS
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    speed: 1000,
+    effect: 'fade',
+    roundLengths: false,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 4000,
+    },
+    // cubeEffect: {
+    //     shadowOffset: 10,
+    // }
+});
+
+
+$('.slick-slider').slick({
+    // dots: true,
+    // arrows: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 5000,
+});
+
+// AOS
+AOS.init();
